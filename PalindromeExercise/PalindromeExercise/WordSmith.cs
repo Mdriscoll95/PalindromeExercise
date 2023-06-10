cd7696a31d695135c20c2f876b2b6a7af0748d76
Michael Driscoll <michael.driscoll951@gmail.com>
2023-06-09 20:55:04 -0700

    hw

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+
+			var wordCharArr = word.ToCharArray();
+			Array.Reverse(wordCharArr);
+			var reversed = new string(wordCharArr);
+
+			if (word.ToLower() == reversed.ToLower())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("civic", true)]
+        [InlineData("tattarrattat", true)]
+        [InlineData("hey", false)]
+        public void PalindromTest(string word, bool expected)
         {
+            //arrange
+            var wordsmith = new WordSmith();
 
+            //act
+            var actual = wordsmith.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
